3c59d9447153f6a4ffb96ef1e01e72ea28fb1770
AbdullahAlRasheedi <alkw444@gmail.com>
2021-01-07 20:53:05 +0300

    cw-8

--- myscript.js
+++ myscript.js
@@ -3,12 +3,51 @@ let board = [
     ['10', '11', '12'],
     ['20', '21', '22']
 ]
-
-
-
-function press(cellNO) {
-
+let turn = 'X'
+function press(x ,y) {
+    if (turn == 'X') {
+        turn = 'O'
+    }
+    else{
+        turn = 'X'
+    }
     // Do stuff
-    console.log(cellNO)
+    
+    let cell = document.getElementById(`${x}${y}`);
+    cell.innerText = turn;
+    board[x][y] = turn;
+    
+    if(board[0][0] == turn && board[0][1] == turn && board[0][2] == turn){
+        document.getElementById(turn).innerText = `you are win!! ${turn}`;
+        
+    }
+    else if(board[1][0] == turn && board[1][1] == turn && board[1][2] == turn){
+        document.getElementById(turn).innerText = `you are win!! ${turn}`;
+        
+    }
+    else if(board[2][0] == turn && board[2][1] == turn && board[2][2] == turn){
+        document.getElementById(turn).innerText = `you are win!! ${turn}`;
+        
+    }
+    else if(board[0][2] == turn && board[1][1] == turn && board[2][0] == turn){
+        document.getElementById(turn).innerText = `you are win!! ${turn}`;
+        
+    }
+    else if(board[1][0] == turn && board[1][1] == turn && board[2][2] == turn){
+        document.getElementById(turn).innerText = `you are win!! ${turn}`;
+        
+    }
+    else if(board[0][0] == turn && board[1][0] == turn && board[2][0] == turn){
+        document.getElementById(turn).innerText = `you are win!! ${turn}`;
+        
+    }
+    else if(board[0][1] == turn && board[1][1] == turn && board[2][1] == turn){
+        document.getElementById(turn).innerText = `you are win!! ${turn}`;
+        
+    }
+    else if(board[0][2] == turn && board[1][2] == turn && board[2][2] == turn){
+        document.getElementById(turn).innerText = `you are win!! ${turn}`;
+        
+    }
     
 }
